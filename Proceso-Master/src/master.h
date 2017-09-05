dfbffca904a152fbc64b72035b8f413e74c8da87
cmedrano <cristian.ariel.medrano@gmail.com>
2017-09-05 13:47:11 -0300

    creado procesos Worker y Master

---
+++ Proceso-Master/src/master.h
@@ -0,0 +1,16 @@
+/*
+ * master.h
+ *
+ *  Created on: 5/9/2017
+ *      Author: utnso
+ */
+
+#ifndef SRC_MASTER_H_
+#define SRC_MASTER_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#endif /* SRC_MASTER_H_ */
