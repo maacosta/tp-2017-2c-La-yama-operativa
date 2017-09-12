46a0efd6929277f5f7931ca1b0a058a41055f744
macosta <manuel.alejandro.acosta@gmail.com>
2017-09-12 14:44:24 -0300

    Se hace handshake entre master y yama (falta respuesta de yama a master por ok o error)

--- common/protocol.c
+++ common/protocol.c
@@ -2,37 +2,35 @@
 
 #define HEADER_SIZE 6
 
-static size_t send_message(message_t message, socket_t sockfd) {
+static size_t send_message(socket_t sockfd, header_t header) {
 	unsigned char buffer[HEADER_SIZE];
-	size_t size = serial_pack(buffer, "CCL", message.header.process, message.operation, message.size);
+	size_t size = serial_pack(buffer, "CCL", header.process, header.operation, header.size);
 	return socket_send_bytes(buffer, size, sockfd);
 }
 
-size_t protocol_handshake_send(socket_t sockfd, header_t header) {
-	message_t message;
-	message.header = header;
-	message.operation = OP_HANDSHAKE;
-	message.size = 0;
-	return send_message(message, sockfd);
+size_t protocol_handshake_send(socket_t sockfd, process_t process) {
+	header_t header;
+	header.process = process;
+	header.operation = OP_HANDSHAKE;
+	header.size = 0;
+	return send_message(sockfd, header);
 }
 
-static size_t receive_message(message_t message, socket_t sockfd) {
+static size_t receive_message(socket_t sockfd, header_t *header) {
 	unsigned char buffer[HEADER_SIZE];
 	size_t size = socket_receive_bytes(buffer, HEADER_SIZE, sockfd);
 	if(size > 0) {
-		serial_unpack(buffer, "CCL", &message.header.process, &message.operation, &message.size);
+		serial_unpack(buffer, "CCL", &header->process, &header->operation, &header->size);
 	}
 	return size;
 }
 
-size_t protocol_handshake_receive(socket_t sockfd, header_t header) {
-	message_t message = {0};
-	size_t size = receive_message(message, sockfd);
-	if(size > 0 && message.operation == OP_HANDSHAKE) {
-		header = message.header;
-		log_msg_info("Handshake con %s exitoso", message.header.process);
+size_t protocol_handshake_receive(socket_t sockfd, header_t *header) {
+	size_t size = receive_message(sockfd, header);
+	if(size > 0 && header->operation == OP_HANDSHAKE) {
+		log_msg_info("Handshake con %d exitoso", header->process);
 	} else {
-		log_msg_error("Error en handshake con %s", message.header.process);
+		log_msg_error("Error en handshake con %d", header->process);
 	}
 	return size;
 }
--- yamamaster/src/main.c
+++ yamamaster/src/main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 void conectar_con_yama() {
-	socket_t sockfd = socket_connect("ip_yama", "puerto");
+	socket_t sockfd = socket_connect("127.0.0.1", "5000");
 
 	header_t header;
 	header.process = MASTER;
 
-	protocol_handshake_send(sockfd, header);
+	protocol_handshake_send(sockfd, MASTER);
 
 	//enviar operacion de tarea
 
