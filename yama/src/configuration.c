a389e03a2afd0bf1d7984e7eacd6fcd80889c0de
utnso <utnso@utnso40>
2017-09-12 15:13:29 -0300

    Se agrega configuración de yama

--- yama/src/configuration.c
+++ yama/src/configuration.c
@@ -5,4 +5,32 @@
  *      Author: utnso
  */
 
+#include "configuration.h"
+
+static t_config* c;
+
+yama_t *config_leer(const char* path) {
+	if(access(path, F_OK) == -1 ) {
+		log_msg_error("No existe el archivo de configuracion %s", path);
+		exit(EXIT_FAILURE);
+	}
+
+	c = config_create((char *)path);
+	yama_t *config = malloc(sizeof(yama_t));
+
+	config->yama_ip 	= config_get_string_value(c, "IP");
+	config->yama_puerto = config_get_int_value(c, "PUERTO");
+	config->yama_retardo_planificacion = config_get_int_value(c, "RETARDO_PLANIFICACION");
+	config->yama_algoritmo_balanceo	= config_get_string_value(c, "ALGORITMO_BALANCEO");
+	config->log_file 	= config_get_string_value(c, "LOG_FILE");
+	config->log_name 	= config_get_string_value(c, "LOG_NAME");
+
+
+	return config;
+}
+
+void config_liberar(yama_t* config) {
+	config_destroy(c);
+	free(config);
+}
 
--- yama/src/configuration.h
+++ yama/src/configuration.h
@@ -8,6 +8,24 @@
 #ifndef YAMA_CONFIGURATION_H_
 #define YAMA_CONFIGURATION_H_
 
+#include <commons/config.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "../../common/log.h"
 
+// Configuracion
+typedef struct{
+	char* yama_ip;
+	int yama_puerto;
+	int yama_retardo_planificacion;
+	char* yama_algoritmo_balanceo;
+	char* log_file;
+	char* log_name;
+} yama_t;
+
+
+yama_t *config_leer(const char* path);
+
+void config_liberar(yama_t* config);
 
 #endif /* SRC_CONFIGURATION_H_ */
