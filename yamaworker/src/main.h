e14fd4159a4ccb96370576bcd20c4da2662f6ab0
macosta <manuel.alejandro.acosta@gmail.com>
2017-09-27 01:21:45 -0300

    Se agrega configuracion para worker y se escribe logica basica de fork

--- yamaworker/src/main.h
+++ yamaworker/src/main.h
@@ -1,15 +1,11 @@
-/*
- * main.h
- *
- *  Created on: 9/9/2017
- *      Author: utnso
- */
-
 #ifndef MAIN_YAMAWORKER_H_
 #define MAIN_YAMAWORKER_H_
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "../../common/socket.h"
+#include "../../common/protocol.h"
+#include "configuration.h"
 
 
 #endif /* MAIN_YAMAWORKER_H_ */
