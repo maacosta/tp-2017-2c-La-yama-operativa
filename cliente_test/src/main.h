556443f69b3d93fa71f3f65d31d793858a654d96
macosta <manuel.alejandro.acosta@gmail.com>
2017-10-23 01:43:33 -0300

    correccion en cliente_test sobre include

--- cliente_test/src/main.h
+++ cliente_test/src/main.h
@@ -5,20 +5,7 @@
 #include <stdlib.h>
 #include "../../common/socket.h"
 #include "../../common/protocol.h"
-#include "configuration.h"
 #include <string.h>
-
-#define PUERTO_WORKER 5005;
-#define IP_WORK 127.0.0.1;
-/*
-#OP >> WRK Iniciar_Transformacion
-        <archivo_transformacion>
-        <descripcion_bloque>
-            <bloque>
-            <bytes_ocupados>
-            <nombre_archivo_temporal>
-*/
-
 
 
 socket_t conectar_con_worker_prueba();
