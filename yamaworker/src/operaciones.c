7cc0f5d80cc8a1021932afc87a868d5d0217c6c8
macosta <manuel.alejandro.acosta@gmail.com>
2017-12-08 17:21:23 -0300

    se completa respuesta a master de worker operaciones: transformacion y reduccion

--- yamaworker/src/operaciones.c
+++ yamaworker/src/operaciones.c
@@ -15,7 +15,7 @@ bool op_transformar(packet_t *packet, socket_t sockMaster, yamaworker_t* config)
 	packet_t paquete;
 	size_t size;
 
-	log_msg_info("operaciones | Etapa Transformacion: archivo [ %d ]", sockMaster);
+	log_msg_info("operaciones | Etapa Transformacion: socket master [ %d ]", sockMaster);
 
 	//recibir informacion de bloque y nombre archivo final temporal
 	int num_bloque, bytes_ocupados, i_es_txt;
@@ -50,19 +50,31 @@ bool op_transformar(packet_t *packet, socket_t sockMaster, yamaworker_t* config)
 	//ejecutar archivo temporal ya guardado localmente
 	char *cmd = string_from_format("cat %s | %s | sort > %s", path_bloque, path_script, path_resultado);
 	log_msg_info("operaciones | Comando a ejecutar [ %s ]", cmd);
-
+	//asignar permisos a script transformacion
 	chmod(path_script, S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH);
 
-	system(cmd);
-	/*
-	char *argv[] = { NULL };
-	char *envp[] = { NULL };
-	if(execve(cmd, argv, envp) == -1) {
+	resultado_t resultado;
+	if(system(cmd) == -1) {
+		resultado = RESULTADO_Error;
 		log_msg_error("operaciones | No se pudo ejecutar el comando [ %s ]", strerror(errno));
-	}*/
+	}
+	resultado = RESULTADO_OK;
+
+	free(path_bloque);
+	free(path_script);
+	free(path_resultado);
 	free(cmd);
 
-	return (EXIT_SUCCESS);
+	//responder a master
+	char buffer[RESPUESTA_SIZE];
+	size = serial_string_pack(&buffer, "h", resultado);
+	cabecera = protocol_get_header(OP_WRK_Iniciar_Transformacion, size);
+	paquete = protocol_get_packet(cabecera, &buffer);
+	if(!protocol_packet_send(sockMaster, &paquete)) {
+		return false;
+	}
+
+	return true;
 }
 
 bool op_reduccion(packet_t *packet, socket_t sockMaster, yamaworker_t* config) {
@@ -70,7 +82,7 @@ bool op_reduccion(packet_t *packet, socket_t sockMaster, yamaworker_t* config) {
 	packet_t paquete;
 	size_t size;
 
-	log_msg_info("operaciones | Etapa Reduccion: archivo [ %d ]", sockMaster);
+	log_msg_info("operaciones | Etapa Reduccion: socket master [ %d ]", sockMaster);
 
 	//recibir informacion de archivos reducidos y nombre archivo final temporal
 	char nombre_archivos_tmp[NOMBRE_ARCHIVO_TMP*10];
@@ -86,8 +98,10 @@ bool op_reduccion(packet_t *packet, socket_t sockMaster, yamaworker_t* config) {
 		char *path = string_from_format("./%s/%s", config->path_tmp, a);
 		string_append_with_format(&cmd, "%s ", path);
 		free(path);
+		free(a);
 	}
 	string_iterate_lines(arc_reduccion_lista, (void *)iterar);
+	free(arc_reduccion_lista);
 
 	//recibir archivo temporal y escribirlo a disco con un nombre temporal
 	paquete = protocol_packet_receive(sockMaster);
@@ -99,25 +113,35 @@ bool op_reduccion(packet_t *packet, socket_t sockMaster, yamaworker_t* config) {
 	guardar_archivo_tmp(config->path_tmp, &nombre_script_tmp, paquete.payload, paquete.header.size, es_txt);
 	protocol_packet_free(&paquete);
 
+	//ejecutar reduccion
 	char *path_script = string_from_format("./%s/%s", config->path_tmp, nombre_script_tmp);
 	char *path_resultado = string_from_format("./%s/%s", config->path_tmp, nombre_archivo_reduccion_local);
 
-	//ejecutar transformacion
 	string_append_with_format(&cmd, "| %s > %s", path_script, path_resultado);
 	log_msg_info("operaciones | Comando a ejecutar [ %s ]", cmd);
 
-	//ejecutar archivo temporal ya guardado localmente
+	//asignar permisos a script reductor
 	chmod(path_script, S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH);
 
-	free(path_script);
+	resultado_t resultado;
+	if(system(cmd) == -1) {
+		resultado = RESULTADO_Error;
+		log_msg_error("operaciones | No se pudo ejecutar el comando [ %s ]", strerror(errno));
+	}
+	resultado = RESULTADO_OK;
 
-	system(cmd);
-	/*
-	char *argv[] = { NULL };
-	char *envp[] = { NULL };
-	execve(cmd, argv, envp);
-	*/
+	free(path_script);
+	free(path_resultado);
 	free(cmd);
 
-	return (EXIT_SUCCESS);
+	//responder a master
+	char buffer[RESPUESTA_SIZE];
+	size = serial_string_pack(&buffer, "h", resultado);
+	cabecera = protocol_get_header(OP_WRK_Iniciar_Reduccion, size);
+	paquete = protocol_get_packet(cabecera, &buffer);
+	if(!protocol_packet_send(sockMaster, &paquete)) {
+		return false;
+	}
+
+	return true;
 }
